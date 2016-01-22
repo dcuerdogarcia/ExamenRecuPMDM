73dd99546797cbc31b511a77d4e6690955754433
acarreiragonzalez <acarreiragonzalez@danielcastelao.org>
2016-01-22 12:09:10 +0100

    Cuarto commit Hice el método del botón onCreateView del ItemDetailFragment

---
+++ app/src/main/java/com/example/dcuerdogarcia/examenrecupdm/ItemDetailFragment.java
@@ -0,0 +1,74 @@
+package com.example.dcuerdogarcia.examenrecupdm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+
+import com.example.dcuerdogarcia.examenrecupdm.dummy.DummyContent;
+
+
+
+public class ItemDetailFragment extends Fragment {
+
+    public static final String ARG_ITEM_ID = "item_id";
+
+    private DummyContent.DummyItem mItem;
+
+    public ItemDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
+
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
+            Button boton = (Button) rootView.findViewById(R.id.botonBorrar);
+            boton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ItemListFragment fragment = (ItemListFragment) getFragmentManager().findFragmentById(R.id.item_list);
+                    if (fragment == null || !fragment.isInLayout()) {
+                        Intent intent = new Intent();
+                        getActivity().setResult(Activity.RESULT_OK, intent);
+                        getActivity().finish();
+                    } else {
+                        ((TextView) rootView.findViewById(R.id.item_detail)).setText("");
+                    }
+                }
+            });
+        }
+
+
+        return rootView;
+    }
+}
